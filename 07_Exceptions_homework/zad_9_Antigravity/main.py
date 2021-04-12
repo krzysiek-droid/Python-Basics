b2f3924b76e2b7a0d7212719e8bc37b68cee2b76
Krzysztof Fryzowicz <goldenbullet1142@gmail.com>
2021-04-12 22:33:08 +0200

    changed condition for https -> moved 'www' to 2nd conditioning block

--- 07_Exceptions_homework/zad_9_Antigravity/main.py
+++ 07_Exceptions_homework/zad_9_Antigravity/main.py
@@ -6,7 +6,7 @@ try:
     if adress.count('/') > 0:
         if adress.count('/') == 2:
             array_adress = adress.split('//')
-            if array_adress[0] != 'http:' and array_adress != 'http:' and array_adress != 'www':
+            if array_adress[0] != 'https:' and array_adress != 'http:':
                 raise ValueError
             else:
                 if array_adress[len(array_adress) - 1] != 'pl' and array_adress[len(array_adress) - 1] != 'com':
@@ -19,6 +19,8 @@ try:
     else:
         if adress.count('.') > 0:
             array_adress = adress.split('.')
+            if array_adress[0] != 'www':
+                raise ValueError
             if array_adress[len(array_adress) - 1] != 'pl' and array_adress[len(array_adress) - 1] != 'com':
                 raise ValueError
             else:
