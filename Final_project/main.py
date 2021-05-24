47acb637e534d61afb1e427157e86b635e5c1bc3
Krzysztof Fryzowicz <goldenbullet1142@gmail.com>
2021-05-24 21:27:35 +0200

    screenmanager added

--- Final_project/main.py
+++ Final_project/main.py
@@ -1,40 +1,67 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivy.lang import Builder
 from kivy.uix.widget import Widget
-from kivy.uix.filechooser import FileChooserController
+from kivy.uix.popup import Popup
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+# requirement for kivy version
 kivy.require("2.0.0")
 
-#Odczyt pliku layout opisującego wygląd aplikacji
-#Builder.load_file('Porosity.kv')
+# referance to the application design file (.kv)
+# Builder.load_file('Porosity.kv')
 
-class PorosityLayout(Widget):
-    def selected(self, filename):
+# Prime window character
+Window.size = (750, 500)
+
+
+# Screen with algorythm chose
+class Select_alg(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
+
+
+# screen with filechooser
+class Filechooser(Screen):
+    def __init__(self):
+        super(Filechooser, self).__init__()
+        self.filepath = " "
+        self.is_selected = False
+
+    def select(self, *args):
         try:
-            self.ids.my_image.source = filename[0]
+            self.filepath = self.ids.filename.text = args[1][0]
         except:
             pass
 
-class ConnectPage(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 2
+    def save_path(self):
+        try:
+            self.is_selected = True
+            self.filepath = self.ids.filename.text
+            print(self.filepath)
+        except:
+            pass
 
-        # self.add_widget(Label(text="Wczytaj dane"))
-        # self.LoadButton = Button(text="Load Data from csv")
-        # self.LoadButton.bind(on_click=self.LoadData)
-        # self.add_widget(self.LoadButton)
 
-    #def LoadData(self, instance):
+# first screen poping up when initilized
+class First_window(Screen):
+    pass
 
-class Porosity(App):
+class PorosityApp(App):
     def build(self):
-        return PorosityLayout()
+        screen_manager = ScreenManager()
+        screen_manager.add_widget(First_window(name='first_window'))
+        file_chooser = Filechooser()
+        screen_manager.add_widget(file_chooser)
+        algorithm = Select_alg()
+        screen_manager.add_widget(algorithm)
 
+        return screen_manager
 
 
 if __name__ == '__main__':
-    Porosity().run()
+    PorosityApp().run()
--- Final_project/porosity.kv.py
+++
@@ -1,9 +0,0 @@
-#:kivy 2.0.0
-<PorosityLayout>
-    BoxLayout:
-        id: porosity_widget
-        orientation: "vertical"
-        size: root.width, root.height
-
-        padding: 50
-        spacing: 20
