bad9b5c3166a3b6a3e041e13d12f193c1c574fd8
Krzysztof Fryzowicz <goldenbullet1142@gmail.com>
2021-05-27 23:13:38 +0200

    update - histogram_screen failure

--- Final_project/main.py
+++ Final_project/main.py
@@ -1,8 +1,9 @@
 import kivy
-import sorting_algorithms_database as sab
+import sorting_algorithms_database as sad
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import time
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -18,9 +19,11 @@ Window.size = (750, 500)
 # first screen popping up when app initialized
 class Welcome_screen(Screen):
     def open_filechooser(self):
+        # initialized filechooser of Windows type (thanks to plyer lib.)
         path = filechooser.open_file(title="Pick a .csv file",
-                                     filtes=[("Comma-separated values", "*.csv")])
+                                     filters=[("Comma-separated values", "*.csv")])
         return path[0]
+
 
 # Algorithm choice screen
 class sorting_algorithms(Screen):
@@ -38,37 +41,60 @@ class sorting_algorithms(Screen):
 
 class WindowManager(ScreenManager):
     filepath = ''
-    choosen_algorithm = ''
+    chosen_algorithm = ''
     pass
 
 
+def load_values_from_csv(filepath):
+    fopen = pd.read_csv(filepath, sep=",")
+    values = fopen.get("Circ.")
+    values = list(values)
+
+    return values
+
+
 class Histogram_screen(Screen):
-    def __init__(self, filepath, algorithm):
+    def __init__(self):
         super(Histogram_screen, self).__init__()
-        #filepath to .csv file with data to be plotted
-        self.file = filepath
-        #name of algorithm used for sorting of data
-        self.selected_algorithm = algorithm
+        self.sorting_time = None
+        self.data = None
 
-    def load_values_from_csv(self, column_name:str):
-        fop = pd.read_csv(self.filepath, sep=",")
-        circ_values = fop.get(column_name)
-        circ_values = list(circ_values)
-        return circ_values
+    def sort_values(self, filepath, selected_algorithm):
 
-    def sort_values(self):
+        values = load_values_from_csv(filepath)
+        print(values)
+
+        if selected_algorithm == "bubble_sort":
+            t0 = time.time()
+            sorted_values = sad.bubble_sort(values)
+        elif selected_algorithm == "merge_sort":
+            t0 = time.time()
+            sorted_values = sad.merge_sort(values)
+        elif selected_algorithm == "insertion_sort":
+            t0 = time.time()
+            sorted_values = sad.insertion_sort(values)
+        else:
+            t0 = time.time()
+            sorted_values = sad.selection_sort(values)
+
+        t1 = time.time()
+        sorting_time = t1 - t0
+        self.sorting_time = round(sorting_time, 5)
+        self.data = sorted_values
 
 
-
-# App logic
+    # App logic
 class PorosityApp(App):
     def build(self):
         screen_manager = WindowManager()
 
-        screen_manager.add_widget(Welcome_screen(name='welcome_screen'))
+        screen_manager.add_widget(screen=Welcome_screen(name='welcome_screen'))
 
         algorithm = sorting_algorithms()
-        screen_manager.add_widget(algorithm)
+        screen_manager.add_widget(screen=algorithm)
+
+        histogram = Histogram_screen()
+        screen_manager.add_widget(screen=histogram)
 
         return screen_manager
 
--- Final_project/test.py
+++ Final_project/test.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+import sorting_algorithms_database as sad
+import time
 from plyer import filechooser
+
+
 # set mean and sd
 # mu, sigma = 100, 15
 #
@@ -13,14 +16,51 @@ from plyer import filechooser
 # n, bins, patches = plt.hist(x, 50)
 # plt.show()
 
+class Histogram_screen:
+    def __init__(self, algorithm):
+        super(Histogram_screen, self).__init__()
+        # filepath to .csv file with data to be plotted
+        # self.file = filepath
+        # name of algorithm used for sorting of data
+        self.selected_algorithm = algorithm
+        self.sorting_time = time.time()
 
+    def sort_values(self, values):
+        sorted = []
 
+        # removing repeated values
+        values = set(values)
+        values = list(values)
 
-# path = filechooser.open_file(title="Pick a CSV file..",
-#                              filters=[("Comma-separated Values", "*.csv")])
-# print(path)
+        if self.selected_algorithm == "bubble_sort":
+            t0 = time.time()
+            sorted = sad.bubble_sort(values)
+        elif self.selected_algorithm == "merge_sort":
+            t0 = time.time()
+            sorted = sad.merge_sort(values)
+        elif self.selected_algorithm == "insertion_sort":
+            t0 = time.time()
+            sorted = sad.insertion_sort(values)
+        else:
+            t0 = time.time()
+            sorted = sad.selection_sort(values)
+
+        t1 = time.time()
+        sorting_time = t1 - t0
+        self.sorting_time = round(sorting_time, 5)
+
+        return sorted
+
 
 fopen = pd.read_csv('H11_PB1.1.csv', sep=";")
 circ_values = fopen.get("Circ.")
+
+print(type(circ_values))
+
+circ_values = set(circ_values)
 circ_values = list(circ_values)
-print(circ_values)
+
+histogram = Histogram_screen("merge_sort")
+
+print(histogram.sort_values(circ_values))
+print(histogram.sorting_time)
